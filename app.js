51f5694767f86aaa496baac16e418ad06b13c3a7
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-04-24 15:38:02 +0530

    added login and register routes

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
+// Home page
 app.get('/', (req, res) => {
-  res.send('My server is running')
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/login.html'))
+})
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/register.html'))
 })
 
 app.listen(3000)
